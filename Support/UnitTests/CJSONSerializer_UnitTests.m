1b9f51c96676c8c9fd3f53f2e8aa49fd48ef08be
Jonathan Wight <jwight@mac.com>
2010-10-15 00:26:34 -0700

    Updated unit tests for the renaming.

--- Support/UnitTests/CJSONSerializer_UnitTests.m
+++ Support/UnitTests/CJSONSerializer_UnitTests.m
@@ -29,44 +29,44 @@
 
 #import "CJSONSerializer_UnitTests.h"
 
-#import "CJSONStringSerializer.h"
+#import "CJSONSerializer.h"
 
 @implementation CJSONSerializer_UnitTests
 -(void)testEmptyDictionary {
 	NSString *jsonEquivalent = @"{}";
 	NSDictionary *emptyDictionary = [NSDictionary dictionary];
-	id theObject = [[CJSONStringSerializer serializer] serializeObject:emptyDictionary error:nil];
-	STAssertEqualObjects(jsonEquivalent, theObject, nil);
+	id theObject = [[CJSONSerializer serializer] serializeObject:emptyDictionary error:nil];
+	STAssertEqualObjects([jsonEquivalent dataUsingEncoding:NSUTF8StringEncoding], theObject, nil);
 }
 
 -(void)testSingleKeyValuePair {
 	NSString *jsonEquivalent = @"{\"a\":\"b\"}";
 	NSDictionary *dictionary = [NSDictionary dictionaryWithObject:@"b" forKey:@"a"];
-	id theObject = [[CJSONStringSerializer serializer] serializeObject:dictionary error:nil];
-	STAssertEqualObjects(jsonEquivalent, theObject, nil);
+	id theObject = [[CJSONSerializer serializer] serializeObject:dictionary error:nil];
+	STAssertEqualObjects([jsonEquivalent dataUsingEncoding:NSUTF8StringEncoding], theObject, nil);
 }
 
 /* http://code.google.com/p/touchcode/issues/detail?id=52 */
 -(void)test_SerializeArrayWithDuplicatedInstances {
     NSString *a = @"a";
     NSArray  *input = [NSArray arrayWithObjects:a, @"b", a, nil];
-    NSString *expected = @"[\"a\",\"b\",\"a\"]";
-    id output = [[CJSONStringSerializer serializer] serializeObject:input error:nil];
+    NSData *expected = [@"[\"a\",\"b\",\"a\"]" dataUsingEncoding:NSUTF8StringEncoding];
+    id output = [[CJSONSerializer serializer] serializeObject:input error:nil];
     STAssertEqualObjects(expected, output, nil);
 }
 
 - (void)testFalse {
 	NSString *jsonEquivalent = @"{\"a\":false}";
 	NSDictionary *dictionary = [NSDictionary dictionaryWithObject:[NSNumber numberWithBool:NO] forKey:@"a"];
-	id theObject = [[CJSONStringSerializer serializer] serializeObject:dictionary error:nil];
-	STAssertEqualObjects(jsonEquivalent, theObject, nil);
+	id theObject = [[CJSONSerializer serializer] serializeObject:dictionary error:nil];
+	STAssertEqualObjects([jsonEquivalent dataUsingEncoding:NSUTF8StringEncoding], theObject, nil);
 }
 
 - (void)testDoubleAccuracy {
 	NSString *jsonEquivalent = @"{\"a\":1.23456789}";
 	NSDictionary *dictionary = [NSDictionary dictionaryWithObject:[NSNumber numberWithDouble:1.23456789] forKey:@"a"];
-	id theObject = [[CJSONStringSerializer serializer] serializeObject:dictionary error:nil];
-	STAssertEqualObjects(jsonEquivalent, theObject, nil);
+	id theObject = [[CJSONSerializer serializer] serializeObject:dictionary error:nil];
+	STAssertEqualObjects([jsonEquivalent dataUsingEncoding:NSUTF8StringEncoding], theObject, nil);
 }
 
 @end
