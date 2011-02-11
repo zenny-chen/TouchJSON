49cdea5119ed33dd963362c36fa13b54542e2919
Jonathan Wight <jwight@mac.com>
2011-02-11 13:52:26 -0800

    Added a copy phase that copies test data to build loc.
    Added a tidied twitter public timeline file and a minimal test case for
    Tom Harrington's bug #3.

--- Support/Demo/main.m
+++ Support/Demo/main.m
@@ -29,10 +29,13 @@
 
 #import <Foundation/Foundation.h>
 
+#import "CJSONSerializer.h"
 #import "CJSONDeserializer.h"
 #import "CJSONScanner.h"
 
-void test(void);
+static void test(void);
+static void test_twitter_public_timeline(void);
+
 
 int main(int argc, char **argv)
 	{
@@ -40,29 +43,29 @@ int main(int argc, char **argv)
 
 	NSAutoreleasePool *theAutoreleasePool = [[NSAutoreleasePool alloc] init];
 
-	test();
+    test_twitter_public_timeline();
 
 	[theAutoreleasePool release];
 	//
 	return(0);
 	}
 
-void test(void)
+static void test(void)
+    {
+    }
+
+static void test_twitter_public_timeline(void)
 	{
-	CJSONDeserializer *theDeserializer = [CJSONDeserializer deserializer];
-	NSString *jsonString = @"3.14";
-	NSData *jsonData = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
-	NSError *error = nil;
-	NSNumber *theNumber = [theDeserializer deserialize:jsonData error:&error];
-
-//    CJSONDeserializer *theDeserializer = [CJSONDeserializer deserializer];
-//    NSData *theData = [@"14399073641566209" dataUsingEncoding:NSUTF8StringEncoding];
-//	NSNumber *theObject = [theDeserializer deserialize:theData error:nil];
-//	STAssertEquals([theObject unsignedLongLongValue], 14399073641566209ULL, @"Numbers did not contain expected contents");
-
-
-	NSLog(@"Result: %@", [theNumber class]);
-	NSLog(@"Error: %llu", [theNumber unsignedLongLongValue]); 
-	NSLog(@"Error: %g", [theNumber doubleValue]); 
-	NSLog(@"Error: %d", [theNumber unsignedLongLongValue] == 14399073641566209ULL); 
+    NSError *theError = NULL;
+    NSData *inputData = [NSData dataWithContentsOfFile:@"Test Data/atomicbird.json"];
+    NSLog(@"Input data: %ld", inputData.length);
+    id json = [[CJSONDeserializer deserializer] deserialize:inputData error:&theError];
+    NSLog(@"JSON Object: %@ %p (Error: %@)", [json class], json, theError);
+    NSData *jsonData = [[CJSONSerializer serializer] serializeObject:json error:&theError];
+    NSLog(@"%@", jsonData);
+    
+    NSLog(@"JSON data: %ld  (Error: %@)", jsonData.length, theError);
+    NSString *jsonString = [[[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding] autorelease];
+    NSLog(@"JSON string: %ld", jsonString.length);
+    NSLog(@"> %@", jsonString);
 	}
