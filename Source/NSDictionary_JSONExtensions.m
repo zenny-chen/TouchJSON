649af6c93f162f452e5a922875b6775d93e679e7
Zenny Chen <zenny_chen@sina.com>
2018-11-05 00:06:12 +0800

    Update NSDictionary_JSONExtensions.m

--- Source/NSDictionary_JSONExtensions.m
+++ Source/NSDictionary_JSONExtensions.m
@@ -28,20 +28,22 @@
 //
 
 #import "NSDictionary_JSONExtensions.h"
-
 #import "CJSONDeserializer.h"
 
 @implementation NSDictionary (NSDictionary_JSONExtensions)
 
-+ (id)dictionaryWithJSONData:(NSData *)inData error:(NSError **)outError
-    {
-    return([[CJSONDeserializer deserializer] deserialize:inData error:outError]);
-    }
++ (instancetype)dictionaryWithJSONData:(NSData *)inData error:(NSError **)outError
+{
+    return([CJSONDeserializer.deserializer deserialize:inData error:outError]);
+}
 
-+ (id)dictionaryWithJSONString:(NSString *)inJSON error:(NSError **)outError;
-    {
++ (instancetype)dictionaryWithJSONString:(NSString *)inJSON error:(NSError **)outError;
+{
     NSData *theData = [inJSON dataUsingEncoding:NSUTF8StringEncoding];
+    
     return([self dictionaryWithJSONData:theData error:outError]);
-    }
+}
 
 @end
+
+
